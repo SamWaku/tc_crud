3c4bce237e97222d1d83d916c896c44e03d8a65d
SamWaku <56162251+SamWaku@users.noreply.github.com>
2023-10-28 21:35:03 +0200

    local

--- server.ts
+++ server.ts
@@ -1,4 +1,20 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+
+dotenv.config()
 const app = express();
 
+app.use(cors({
+  origin: "*"
+}));
+
+app.get("/", (req, res) => {
+  res.send("API running")
+})
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
